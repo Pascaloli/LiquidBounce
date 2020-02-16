51799c111d94873d0284bccc910e29a8ab7a4242
Kawaii Neko Lolis <marco@ccbluex.net>
2020-02-16 05:52:15 +0100

    Setup cross version workspace

---
+++ 1.12.2/src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/client/MixinMinecraft.java
@@ -0,0 +1,22 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.injection.forge.mixins.client;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    @Inject(method = "run", at = @At("HEAD"))
+    private void test(final CallbackInfo callbackInfo) {
+        System.out.println("WELCOME TO LIQUIDBOUNCE 1.12.2 CROSS VERSION!");
+    }
+
+}
